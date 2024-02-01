aab6946a8517f60b75a92021d0f73c8d0d50bc5f
getablow <hnbnge@gmail.com>
2024-02-01 19:40:04 +0900

    meow

--- src/main/java/org/zerock/b01/domain/Board.java
+++ src/main/java/org/zerock/b01/domain/Board.java
@@ -1,4 +1,32 @@
 package org.zerock.b01.domain;
 
-public class Board {
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Board extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //key generate strategy
+    private Long bno;
+
+    @Column(length = 500, nullable = false) //setting length of column and null
+    private String title;
+
+    @Column(length = 2000, nullable = false)
+    private String content;
+
+    @Column(length = 50, nullable = false)
+    private String writer;
+
+    public void change(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
 }
---
+++ src/main/java/org/zerock/b01/repository/search/BoardSearchImpl.java
@@ -0,0 +1,18 @@
+package org.zerock.b01.repository.search;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import org.zerock.b01.domain.Board;
+
+public class BoardSearchImpl extends QuerydslRepositorySupport implements BoardSearch {
+
+    public BoardSearchImpl(){
+        super(Board.class);
+    }
+
+    @Override
+    public Page<Board> search1(Pageable pageable){
+        return null;
+    }
+}
