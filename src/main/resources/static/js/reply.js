dd3a4212e8ea59ac740f07cce18e593d60219f57
getablow <hnbnge@gmail.com>
2024-02-18 19:20:51 +0900

    meow

--- src/main/resources/static/js/reply.js
+++ src/main/resources/static/js/reply.js
@@ -13,5 +13,17 @@ async function getList({bno, page, size, goLast}){
 
     const result = await axios.get(`/api/replies/list/${bno}`, {params: {page, size}})
 
+    if(goLast){
+        const total = result.data.total
+        const lastPage = parseInt(Math.ceil(total/size))
+        return getList({bno:bno, page:lastPage, size:size})
+    }
+
     return result.data
 }
+
+async function addReply(replyObj){
+    const response = await axios.post(`/api/replies/`, replyObj)
+    return response.data
+}
+
