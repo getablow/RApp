d8c34fbac209a5c2d95536321fbc8d04abf8120d
getablow <hnbnge@gmail.com>
2024-03-06 20:35:26 +0900

    meow

--- src/main/java/org/zerock/b01/controller/MemberController.java
+++ src/main/java/org/zerock/b01/controller/MemberController.java
@@ -5,13 +5,19 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.b01.dto.MemberJoinDTO;
+import org.zerock.b01.service.MemberService;
 
 @Controller
 @Log4j2
 @RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
 
     @GetMapping("/login")
     public void loginGET(String error, String logout){
@@ -22,6 +28,33 @@ public class MemberController {
         if(logout != null){
             log.info("user logout............");
         }
+
+    }
+
+    @GetMapping("/join")
+    public void joinGET(String errorCode, String logout) {
+
+        log.info("join get........");
+
+    }
+
+
+    @PostMapping("/join")
+    public String joinPOST(MemberJoinDTO memberJoinDTO, RedirectAttributes redirectAttributes){
+
+        log.info("join post.........");
+        log.info(memberJoinDTO);
+
+        try {
+            memberService.join(memberJoinDTO);
+        } catch (MemberService.MidExistException e){
+            redirectAttributes.addFlashAttribute("error", "mid");
+            return "redirect:/member/join";
+        }
+
+        redirectAttributes.addFlashAttribute("result", "success");
+
+        return "redirect:/member/login"; //회원가입 후 로그인
 
     }
 
--- src/main/java/org/zerock/b01/security/CustomUserDetailsService.java
+++ src/main/java/org/zerock/b01/security/CustomUserDetailsService.java
@@ -4,6 +4,7 @@ package org.zerock.b01.security;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,6 +12,12 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.zerock.b01.domain.Member;
+import org.zerock.b01.repository.MemberRepository;
+import org.zerock.b01.security.dto.MemberSecurityDTO;
+
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 
 @Log4j2
@@ -18,21 +25,44 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
-    //private PasswordEncoder passwordEncoder;
+    private final MemberRepository memberRepository;
+
+    /*private PasswordEncoder passwordEncoder;
     final private BCryptPasswordEncoder passwordEncoder;
     public CustomUserDetailsService(){
         this.passwordEncoder = new BCryptPasswordEncoder();
-    }
+    }*/
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         log.info("loadUserByUsername: " + username);
 
-        UserDetails userDetails = User.builder().username("user1").password(passwordEncoder.encode("1111")).authorities("ROLE_USER").build();
+        Optional<Member> result = memberRepository.getWithRoles(username);
+
+        if(result.isEmpty()){
+            throw new UsernameNotFoundException("username not found....");
+        }
+
+        Member member = result.get();
+
+        MemberSecurityDTO memberSecurityDTO = new MemberSecurityDTO(
+                member.getMid(),
+                member.getMpw(),
+                member.getEmail(),
+                member.isDel(),
+                false,
+                member.getRoleSet().stream().map(memberRole -> new SimpleGrantedAuthority("ROLE_"+memberRole.name())).collect(Collectors.toList())
+        );
+
+        log.info("memberSecurityDTO.........");
+        log.info(memberSecurityDTO);
 
 
-        return userDetails;
+        //UserDetails userDetails = User.builder().username("user1").password(passwordEncoder.encode("1111")).authorities("ROLE_USER").build();
+
+
+        return memberSecurityDTO;
 
     }
 }
